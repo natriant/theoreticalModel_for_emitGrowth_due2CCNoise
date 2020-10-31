16426bebeaaaf7240ac17f19edb6046bb1279d26
natriant <nataliatriantafyllou@gmail.com>
2020-10-31 23:22:05 +0100

    some simplifications in the script

--- cmpt_GrowthDependence_on_BunchLength.py
+++ cmpt_GrowthDependence_on_BunchLength.py
@@ -22,8 +22,6 @@ plt.rc('text', usetex=False)
 plt.rc('font', family='serif')
 plt.rcParams.update(params)
 
-noise_type = 'BOTH'  # options: 'AN', 'PN', 'BOTH'
-
 # the noise levels correspond to the coast2-setting2 ~ coast3-setting1
 PSD_PN, PSD_AN = -111.28, -115.71  # PSD at fb in dBc/Hz
 
@@ -35,34 +33,22 @@ Eb = 270e9  # eV
 beta_0 = 0.999999  # cmpt it from the rest
 gamma_0 = 287.7  # cmt it
 clight = 299792458  # light speed in meters/second
-f_RF = 400.789e6  # CC frequency in Hz
+f_CC_RF = 400.789e6  # CC frequency in Hz
 
 # bunch length scan in rad, according to the paper of Themis&Phillipe Fig.4
 sigma_phi_list = np.linspace(0, 1.6, 100)  # rad
 sigma_phi_list = sigma_phi_list[1:]  # drop the first element bunch length equals zero is not realistic
 
-myC_PN_list = []
-myC_AN_list = []
-dey_PN_list = []
-dey_AN_list = []
+# Compute the correction factors due to the bunch length
+myC_PN_list = cmpt_bunch_length_correction_factor(sigma_phi_list, noise_type='PN')
+myC_AN_list = cmpt_bunch_length_correction_factor(sigma_phi_list, noise_type='AN')
 
-for sigma_phi in sigma_phi_list:
-    myC_PN = cmpt_bunch_length_correction_factor(sigma_phi, noise_type='PN')
-    myC_AN = cmpt_bunch_length_correction_factor(sigma_phi, noise_type='AN')
-    myC_PN_list.append(myC_PN)
-    myC_AN_list.append(myC_AN)
+# Compute the geometric growth in m/s
+dey_PN_list = emit_growth_phase_noise(betay, Vcc, frev, Eb, myC_PN_list, ssb_2_dsb(PSD_PN), True)  # true for one-sided PSD
+dey_AN_list = emit_growth_amplitude_noise(betay, Vcc, frev, Eb, myC_AN_list, ssb_2_dsb(PSD_AN), True)
 
-    dey_PN_list.append(emit_growth_phase_noise(betay, Vcc, frev, Eb, myC_PN, ssb_2_dsb(PSD_PN), True))  # true for one-sided PSD
-    dey_AN_list.append(emit_growth_amplitude_noise(betay, Vcc, frev, Eb, myC_AN, ssb_2_dsb(PSD_AN), True))  # true for
-
-sigma_z_list = []
-for sigma_phi in sigma_phi_list:
-    sigma_z_list.append(bunch_length_rad_to_m(sigma_phi, clight, f_RF))
-
-
-sigma_t_list = []
-for sigma_z in sigma_z_list:
-    sigma_t_list.append(bunch_length_m_to_time(sigma_z, clight))
+sigma_z_list = bunch_length_rad_to_m(sigma_phi_list, clight, f_CC_RF)  # meters
+sigma_t_list = bunch_length_m_to_time(sigma_z_list, clight)  # seconds
 
 fig, ax = plt.subplots(1,1)
 #ax.plot(np.array(sigma_t_list)*4*1e9, (np.array(dey_AN_list)+np.array(dey_PN_list))*1e9*beta_0*gamma_0**1e-3*3600 , '-')
@@ -71,7 +57,7 @@ fig, ax = plt.subplots(1,1)
 #ax.plot(np.array(sigma_t_list)*4*1e9,  np.array(dey_AN_list)*1e9*beta_0*gamma_0*1e-3*3600 , '-')
 ax.plot(np.array(sigma_t_list)*4*1e9,  (np.array(dey_AN_list)+np.array(dey_PN_list))*1e9*beta_0*gamma_0*1e-3*3600, '-', c='k')
 
-
+'''
 sigma_t_points = [(1.71e-9)/4, (2.21e-9)/4,(2.13e-9)/4, (2.1e-9)/4 ]
 #sigma_t_points = [(1.63e-9)/4, (2.15e-9)/4,(2.07e-9)/4, (2.05e-9)/4 ]
 
@@ -86,16 +72,17 @@ for index, my_sigma_t in enumerate(sigma_t_points):
     dey_AN = emit_growth_amplitude_noise(betay, Vcc, frev, Eb, my_C_AN, ssb_2_dsb(PSD_AN), True)
 
     ax.plot(my_sigma_t*4*1e9, (dey_AN+dey_PN)*1e9*beta_0*gamma_0*1e-3*3600, 'o', c='C{}'.format(index), label='bunch {}'.format(index+1))
+'''
 
-ax.legend()
-ax.set_xlabel(r'$4 \sigma _t (ns)$')
-ax.set_ylabel(r'$d \epsilon_y / dt$' + ' ' +r'$\mu/h$')
+#ax.legend()
+ax.set_xlabel(r'$\mathrm{4 \sigma _t (ns)}$')
+ax.set_ylabel(r'$\mathrm{d \epsilon_y / dt}$' + ' ' +r'$\mathrm{\mu/h}$')
 ax.grid(linestyle='--')
 #ax.set_xlim(1.5, 2.5)
 #ax.set_ylim(4.8, 5.5)
 plt.tight_layout()
-#plt.show()
+plt.show()
 
-plt.savefig('./figures/dey_vs_4sigmat_Coast2-Setting2_14_47.png')
+#plt.savefig('./figures/dey_vs_4sigmat_Coast2-Setting2_14_47.png')
 
 
